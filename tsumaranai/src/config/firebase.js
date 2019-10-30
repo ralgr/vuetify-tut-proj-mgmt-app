b1f7117e405389317130146f4064985a52528064
ralgr <2torrk05@solent.ac.uk>
2019-10-30 10:34:07 +0000

    Update firebase.js

--- tsumaranai/src/config/firebase.js
+++ tsumaranai/src/config/firebase.js
@@ -3,12 +3,7 @@ import 'firebase/firestore'
 
 // Initialize Firebase
 var config = {
-  apiKey: "AIzaSyDJGWdlF1LoQUAHJuHkKq8kBpd9kjmkRhM",
-  authDomain: "pussychat-6b1f1.firebaseapp.com",
-  databaseURL: "https://pussychat-6b1f1.firebaseio.com",
-  projectId: "pussychat-6b1f1",
-  storageBucket: "pussychat-6b1f1.appspot.com",
-  messagingSenderId: "849794017584"
+
 };
 
 firebase.initializeApp(config);
